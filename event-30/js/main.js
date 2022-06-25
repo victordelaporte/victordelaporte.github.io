0d3929a785cb048c33e8ee6477aaa2916d2804f6
victor <victor.delaporte4@gmail.com>
2022-06-25 17:44:15 +0200

    Add files via upload

--- event-30/js/main.js
+++ event-30/js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2020/04/07';
+        var finalDate = '2023/05/27';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
