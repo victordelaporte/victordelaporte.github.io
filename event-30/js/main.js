86a09e6e3ce0b82cf0912e04f4d9521afb5f2cd4
victor <victor.delaporte4@gmail.com>
2022-06-25 17:53:00 +0200

    Add files via upload

--- event-30/js/main.js
+++ event-30/js/main.js
@@ -95,7 +95,7 @@
         .on('update.countdown finish.countdown', function(event) {
 
             var str = '<div class=\"top\"><div class=\"time days\">' +
-                      '%D <span>day%!D</span>' + 
+                      '%D <span>jour%!D</span>' + 
                       '</div></div>' +
                       '<div class=\"time hours\">' +
                       '%H <span>H</span></div>' +
